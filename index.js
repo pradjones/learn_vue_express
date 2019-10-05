7256f58a5dec67a32d949ddf2f73d2ea3d115ee4
pradjones <33274485+pradjones@users.noreply.github.com>
2019-10-05 18:45:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express =  require('express');
+const app = express();
 
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static('test'));
+
+app.get('/quiz', function(req, res){
+    res.sendFile(__dirname + '/test/test.html');
+    console.log('sent file');
+  }); 
+
+
+app.listen(5000, ()=> console.log("hello"));
